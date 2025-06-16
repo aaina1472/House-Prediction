3bd9b8a0f4fb4d598fbd320d898cc76117a7aac5
aaina1472 <aainasrivastav@gmail.com>
2025-06-16 16:14:29 +0530

    housepredapp.py

--- housepredapp.py
+++ housepredapp.py
@@ -8,7 +8,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
 # ======== Load Model and Pipeline ========
-model = load('Dragon (1).joblib')  # Your trained Decision Tree model
+model = load('Dragon (2).joblib')  # Your trained Decision Tree model
 
 # IMPORTANT: Feature order must match training
 feature_names = ['CRIM', 'ZN', 'INDUS', 'NOX', 'RM', 'AGE', 'DIS',
